2189de3eabf5086bad04aa57451f98a6bf523ab6
Vladislav <vvllaadd73@gmail.com>
2026-01-12 17:49:39 +0200

    Fix

---
+++ app/notes/[id]/error.tsx
@@ -0,0 +1,11 @@
+// app/notes/[id]/error.tsx
+
+'use client';
+
+type Props = {
+   error: Error;
+};
+
+export default function Error({ error }: Props) {
+   return <p>Could not fetch note details. {error.message}</p>;
+}
